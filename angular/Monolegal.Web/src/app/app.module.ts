90071e5918340f5f66aeb3fde1d905c93562c6fb
Andres Rodriguez <andresroingsis@gmail.com>
2019-05-21 16:42:24 -0500

    Agregado de servicios en angular, y componentes de clientes y facturas

--- angular/Monolegal.Web/src/app/app.module.ts
+++ angular/Monolegal.Web/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClientesComponent } from './vistas/clientes/clientes.component';
+import { FacturasComponent } from './vistas/facturas/facturas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClientesComponent,
+    FacturasComponent
   ],
   imports: [
     BrowserModule,
